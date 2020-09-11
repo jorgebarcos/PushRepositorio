f4902435dc24982be1d2ba17f66bfd6eef1c389d
Jorge Barcos <jorgebarcos88@gmail.com>
2020-09-11 16:13:29 -0300

    notifiaciones

--- push3/src/firebase-messaging-sw.js
+++ push3/src/firebase-messaging-sw.js
@@ -1,17 +1,17 @@
 importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-messaging.js');
 
-var config = {
-    apiKey: "AIzaSyDE2qwKTzImFoNISJdzCl3AfjxYQMtBbSI",
-    authDomain: "notification-push-d46d9.firebaseapp.com",
-    databaseURL: "https://notification-push-d46d9.firebaseio.com",
-    projectId: "notification-push-d46d9",
-    storageBucket: "notification-push-d46d9.appspot.com",
-    messagingSenderId: "633511189905",
-    appId: "1:633511189905:web:2590c91756f65c58308e30"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(config);
+let firebaseConfig = {
+  apiKey: "AIzaSyDE2qwKTzImFoNISJdzCl3AfjxYQMtBbSI",
+  authDomain: "notification-push-d46d9.firebaseapp.com",
+  databaseURL: "https://notification-push-d46d9.firebaseio.com",
+  projectId: "notification-push-d46d9",
+  storageBucket: "notification-push-d46d9.appspot.com",
+  messagingSenderId: "633511189905",
+  appId: "1:633511189905:web:2590c91756f65c58308e30"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
